6b92b6aa0298d216827566f3ddf2fe96420edcf0
DominikaMaciag <64416993+DominikaMaciag@users.noreply.github.com>
2020-06-13 22:48:40 +0200

    Update KlasaDrzewo.cpp

--- KlasaDrzewo.cpp
+++ KlasaDrzewo.cpp
@@ -1,12 +1,12 @@
 #include "KlasaDrzewo.h"
-KlasaDrzewo::KlasaDrzewo(float x, float y)
+KlasaDrzewo::KlasaDrzewo(float x, float y) //definicja konstruktora
 		{	
 		tekstura.loadFromFile("tekstury/drzewo.png");
 		setTexture(tekstura);
 		setPosition(sf::Vector2f(x,y));
 		};
 		
-KlasaDrzewo::KlasaDrzewo (const KlasaDrzewo &drzewko)
+KlasaDrzewo::KlasaDrzewo (const KlasaDrzewo &drzewko) //definicja konstruktora kopiującego
 		{
 		setTexture(*(drzewko.getTexture()));
 		setPosition(drzewko.getPosition());
